aea533811bb100e00fd81e583948899f50e36c7c
Zixuan <liusherry08@gmail.com>
2025-11-22 23:57:47 +0000

    a change

--- routes/adoptionRoute.js
+++ routes/adoptionRoute.js
@@ -3,12 +3,13 @@ const { protect } = require("./../controllers/authController");
 const {
   createAdoptions,
   getAllAdoptions,
+  editAdoption,
 } = require("./../controllers/adoptionController");
 
 const router = express.Router();
 
 router.post("/", protect, createAdoptions);
 router.get("/", protect, getAllAdoptions);
-router.patch("/:adoptionId", protect);
+router.patch("/:adoptionId", protect, editAdoption);
 
 module.exports = router;
